738b5ddff30aaff0d4840191daba31936c879dc5
OfirG <a>
2015-12-03 18:08:03 +0200

    added symbol table interface (still need to implement)

---
+++ src/slp/SymbolTable.java
@@ -0,0 +1,46 @@
+package slp;
+
+public class SymbolTable {
+	
+	public SymbolTable()()
+	{
+		
+	}
+	
+	/*
+	 * is variable declared in current scopt
+	 */
+	bool IsDeclaredInCurrentScope(String name)
+	{
+		
+	}
+	/*
+	 * get the closest variable with the same name as the given name
+	 */
+	Type GetClosestVarWithSameName(String name)
+	{
+		
+	}
+	
+	/*
+	 * starting new scope of a function, block, class, etc
+	 */
+	void StartScope()
+	{
+		
+	}
+	
+	/*
+	 * exiting the scope and returning to the old scope
+	 */
+	void ExitScope()
+	{
+		
+	}
+	
+	void InsertNewDecleration(String name, Type t1)
+	{
+		
+	}
+	
+}
